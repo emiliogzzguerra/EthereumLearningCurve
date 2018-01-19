8b92547cebba73af427c872443e201a6b7443e0a
emiliogzzguerra <emiliogzz24@gmail.com>
2018-01-19 15:40:06 -0600

    reinstalled node, finished testing warmup, resetted the testing mocha framework

--- 30-Boilerplate_Requirements/inbox/test/Inbox.test.js
+++ 30-Boilerplate_Requirements/inbox/test/Inbox.test.js
@@ -1,1 +1,61 @@
 //Interact with contract through js
+
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); // This is the class
+					  //This changes if we want to connect to for example Rinkeby or other ethereum networks
+const web3 = new Web3(ganache.provider()); // This is the instance of Web3
+
+
+class Car {
+	park() {
+		return 'stopped';
+	}
+
+	drive() {
+		return 'vroom';
+	}
+}
+	  // TestID
+describe('CarWithoutBeforeEach', () => {
+	it('can park', () => {
+		const car = new Car();
+					//ValueProducedByCode,ValueWeThinkItShouldBe
+		assert.equal(car.park(), 'stopped');
+	});
+
+	it('can drive', () => {
+		const car = new Car();
+
+		assert.equal(car.drive(), 'vroom');
+	});
+});
+
+let car; //First, undefined
+
+beforeEach(() => {
+	car = new Car(); // Each time this runs, car is assigned a fresh new car
+});
+	  // TestID
+describe('CarWithBeforeEach', () => {
+	it('can park', () => {
+		assert.equal(car.park(), 'stopped');
+	});
+
+	it('can drive', () => {
+		assert.equal(car.drive(), 'vroom');
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
