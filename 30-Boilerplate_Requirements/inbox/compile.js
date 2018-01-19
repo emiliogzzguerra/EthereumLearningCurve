2d51bf4503ffd36962d7ad01df5b644652d070f0
emiliogzzguerra <emiliogzz24@gmail.com>
2018-01-19 15:12:17 -0600

    Completed compile.js

--- 30-Boilerplate_Requirements/inbox/compile.js
+++ 30-Boilerplate_Requirements/inbox/compile.js
@@ -9,5 +9,5 @@ const solc = require('solc'); // Solidity compiler
 const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
 
